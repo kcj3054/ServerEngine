a043f6b856d229b0c106026d46d2805126211d55
changjukim <kcj3054@gmail.com>
2024-06-09 02:42:36 +0900

    비동기 정지 추석 해제

--- C#_Socket/Server/Program.cs
+++ C#_Socket/Server/Program.cs
@@ -13,5 +13,5 @@ Listener _listener = new();
 _listener.Init(endPoint, () => { return new ClientSession(); });
 Console.WriteLine("Listening...");
 
-//await Task.Delay(Timeout.Infinite);  // 비동기로 막는 방식 더 cpu가 덜 소모된다. 
+await Task.Delay(Timeout.Infinite);  // 비동기로 막는 방식 더 cpu가 덜 소모된다. 
 // Console.ReadLine();  // 동기로 막는 방식.. 
