7950176c071b4681a94df6e1d6502909cc9f4499
changjukim <kcj3054@gmail.com>
2024-06-09 15:31:35 +0900

    빌드 후 자동으로 .bat 실행하도록 visual studio build event 설정

--- C#_Socket/DummyClient/ServerSession.cs
+++ C#_Socket/DummyClient/ServerSession.cs
@@ -49,29 +49,7 @@ namespace DummyClient
             PlayerInfoReq packet = new PlayerInfoReq() { size = 4, packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
 
 
-            // 보낸다
-            for (int i = 0; i < 5; i++)
-            {
-                ArraySegment<byte> s = SendBufferHelper.Open(4096);
-                //byte[] size = BitConverter.GetBytes(packet.size);
-                //byte[] packetId = BitConverter.GetBytes(packet.packetId);
-                //byte[] playerId = BitConverter.GetBytes(packet.playerId);
-
-                ushort size = 0;
-                bool success = true;
-
-                size += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + size, s.Count - size), packet.packetId);
-                size += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + size, s.Count - size), packet.playerId);
-                size += 8;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), size);
-
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(size);
-
-                if (success)
-                    Send(sendBuff);
-            }
+            //패킷 테스트해서 보내기 
         }
 
 
